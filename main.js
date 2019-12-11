539b39f4319c3c5f814744ae6687cc85c25a6d7c
Scott Tsuchiyama <scott.k.tsuchiyama@gmail.com>
2019-12-11 14:36:13 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+module.exports = {
+  rules: {
+    "no-multiple-empty-lines": [0, false],
+    "brace-style": [2, "stroustrup"],
+    "generator-star-spacing": [2, {
+      "before": false,
+      "after": true,
+      "anonymous": "neither",
+      "method": {
+        "before": true,
+        "after": true
+      }
+    }],
+    "indent": [2, 2],
+    "quotes": [2, "double"],
+    "semi": [2, "always"],
+    "semi-style": [2, "last"],
+    "space-before-function-paren": [0, false]
+  }
+};
+
