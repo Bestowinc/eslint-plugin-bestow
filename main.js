6673d2af8cfa1ebaec759cfe63fe45eeffc5da64
Scott Drake <demonburrito@gmail.com>
2019-12-12 01:27:13 -0600

    REWORK: Added separate `config`s
    This one works.
    
    When referred to in a project's `.eslintrc.json`, it should look like:
    ```json
    "extends": [
      "standard",
      "plugin:bestow/parity"
    ],
    "plugins": [
    	"jest",
    	"bestow"
    ]
    ```
    (e.g., in `steam-enroller`'s `.eslintrc.json`, other keys omitted for
    brevity.)
    
    For further refixes, this is our guide (FWIW):
    https://eslint.org/docs/developer-guide/working-with-plugins

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 module.exports = {
-  extends: "eslint-config-standard",
-  rules: {
-    "no-multiple-empty-lines": [0, false],
-    "brace-style": [2, "stroustrup"],
-    "generator-star-spacing": [2, {
-      "before": false,
-      "after": true,
-      "anonymous": "neither",
-      "method": {
-        "before": true,
-        "after": true
+  configs: {
+    parity: {
+      rules: {
+        "no-multiple-empty-lines": [0, false],
+        "brace-style": [2, "stroustrup"],
+        "generator-star-spacing": [2, {
+          "before": false,
+          "after": true,
+          "anonymous": "neither",
+          "method": {
+            "before": true,
+            "after": true
+          }
+        }],
+        "indent": [2, 2],
+        "quotes": [2, "double"],
+        "semi": [2, "always"],
+        "semi-style": [2, "last"],
+        "space-before-function-paren": [0, false]	
       }
-    }],
-    "indent": [2, 2],
-    "quotes": [2, "double"],
-    "semi": [2, "always"],
-    "semi-style": [2, "last"],
-    "space-before-function-paren": [0, false]
+    }
   }
 };
-
