cf0dcb929a11a1e2e37b56d2b21ce47fa7977cef
JP Angelle <jp@bestow.co>
2020-03-04 16:34:09 -0600

    remove parity ruleset

--- main.js
+++ main.js
@@ -1,29 +1,5 @@
 module.exports = {
   configs: {
-    parity: {
-      rules: {
-        "no-multiple-empty-lines": [0, false],
-        "brace-style": [2, "stroustrup"],
-        "generator-star-spacing": [
-          2,
-          {
-            before: false,
-            after: true,
-            anonymous: "neither",
-            method: {
-              before: true,
-              after: true
-            }
-          }
-        ],
-        indent: [2, 2],
-        quotes: [2, "double"],
-        semi: [2, "always"],
-        "semi-style": [2, "last"],
-        "space-before-function-paren": [0, false]
-      }
-    },
-
     twentyTwenty: {
       env: {
         browser: true,
